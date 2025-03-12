ca09df3ad7c3940a3498316e93cab89e057bac1e
Raphal Luis <raphluis11@gmail.com>
2025-03-12 09:42:31 +0100

    .

--- ArgentBankiSite/src/pages/user.jsx
+++ ArgentBankiSite/src/pages/user.jsx
@@ -1,5 +1,6 @@
 import '../styles/user.css'
-export default function User() {
+import Account from '../componets/accounts/accounts'
+export default function User({ user }) {
   return (
     <>
       <main className='main bg-dark '>
@@ -7,41 +8,29 @@ export default function User() {
           <h1>
             Welcome back
             <br />
-            Tony Jarvis!
+            {user}Tony Jarvis!
           </h1>
           <button className='edit-button'>Edit Name</button>
         </div>
         <h2 className='sr-only'>Accounts</h2>
-        <section className='account'>
-          <div className='account-content-wrapper'>
-            <h3 className='account-title'>Argent Bank Checking (x8349)</h3>
-            <p className='account-amount'>$2,082.79</p>
-            <p className='account-amount-description'>Available Balance</p>
-          </div>
-          <div className='account-content-wrapper cta'>
-            <button className='transaction-button'>View transactions</button>
-          </div>
-        </section>
-        <section className='account'>
-          <div className='account-content-wrapper'>
-            <h3 className='account-title'>Argent Bank Savings (x6712)</h3>
-            <p className='account-amount'>$10,928.42</p>
-            <p className='account-amount-description'>Available Balance</p>
-          </div>
-          <div className='account-content-wrapper cta'>
-            <button className='transaction-button'>View transactions</button>
-          </div>
-        </section>
-        <section className='account'>
-          <div className='account-content-wrapper'>
-            <h3 className='account-title'>Argent Bank Credit Card (x8349)</h3>
-            <p className='account-amount'>$184.30</p>
-            <p className='account-amount-description'>Current Balance</p>
-          </div>
-          <div className='account-content-wrapper cta'>
-            <button className='transaction-button'>View transactions</button>
-          </div>
-        </section>
+        <Account
+          title='Argent Bank Checking'
+          number='x8349'
+          amount='2,082.79'
+          description='Available Balance'
+        />
+        <Account
+          title='Argent Bank Savings'
+          number='x6712'
+          amount='10,928.42'
+          description='Available Balance'
+        />
+        <Account
+          title='Argent Bank Credit Card'
+          number='x8349'
+          amount='184.30'
+          description='Current Balance'
+        />{' '}
       </main>{' '}
     </>
   )
